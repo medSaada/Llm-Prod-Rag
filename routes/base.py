e8a18ba70f26fb3ada2d7ab63e1e2c32e665d0a5
Saada <amine@eyepick.com>
2025-04-19 21:01:15 +0200

    the app is ready with a new prefix

--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, APIRouter
-
+import os
 
 #we define the routes here 
-router = APIRouter()    
+router = APIRouter(
+    prefix="/api",
+    tags=["app_v1"]
+)    
 
 @router.get("/")
 def root():
-    return {"message": "Hello World"}
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    return {
+        "app_name": app_name,
+        "app_version": app_version
+        }
